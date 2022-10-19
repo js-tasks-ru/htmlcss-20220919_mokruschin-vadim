2142855836f99dfe1e9abbbd3fc3398287f6f2c3
Vadim Mokrushin <mokr.vo@yandex.ru>
2022-10-19 17:19:43 +0700

    Theme Changer

---
+++ assets/scripts/burger-menu.js
@@ -0,0 +1,21 @@
+const menu = document.getElementById('burger-menu');
+const menuClose = document.getElementById('burger-menu-close');
+const menuOpen = document.getElementById('burger-menu-open');
+
+if (menuOpen) {
+    menuOpen.addEventListener('click', function() {
+        if (menu) {
+            menu.setAttribute('data-open', '');
+            console.log('ss')
+        }
+    });
+}
+
+if (menuClose) {
+    menuClose.addEventListener('click', function() {
+        if (menu) {
+            menu.removeAttribute('data-open');
+            console.log('ss')
+        }
+    });
+}
